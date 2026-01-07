a6a61de9b8ec1763e6f62359a5c1a0366af9197b
Awab <awab@dhcp-130-58-98-239.swarthmore.edu>
2026-01-07 16:26:24 -0500

    mak ai endpoint and also fix the mess in the end

--- app.py
+++ app.py
@@ -1,28 +1,67 @@
 from flask import Flask, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
-from transformers import pipeline 
+from transformers import T5ForConditionalGeneration, T5Tokenizer 
 import json
 
-# app = Flask(__name__)
 
-# @app.route("/main", methods = ["GET"])
-# def main():
-#     return jsonify(message= "This is our main page")
-
-
+model = T5ForConditionalGeneration.from_pretrained("t5-base")
+model.eval()
+tokenizer = T5Tokenizer.from_pretrained("t5-base")
 
  
 def get_transcript(videoID):
     text = []
     yt_api = YouTubeTranscriptApi()
     fetchedTranscript = yt_api.fetch(videoID)
+
     for snippet in fetchedTranscript:
         text.append(snippet.text)
+
     transcript = " ".join(text)
     return transcript
 
 
-print(get_transcript("O1pL5Anel80"))
+def summarized_transcript(transcript):
+
+    inputs = tokenizer.encode(
+        "summarize: " + transcript, 
+        return_tensors="pt", 
+        max_length=512, 
+        truncation=True
+    )
+
+    tokens = model.generate(
+        inputs, 
+        max_length = 150,
+        length_penalty = 1.5,
+        num_beams = 8,
+        early_stopping = True                                                         
+    )
+    summary = tokenizer.decode(tokens[0], skip_special_tokens=True)
+
+    return summary
+
+
+app = Flask(__name__)
+
+@app.route("/api/summarize?youtube_url=<url>")
+def YT(url):
+    YtvideoID = request.args.get("youtube_url")
+    transcript = get_transcript(YtvideoID)
+    summary = summarized_transcript(transcript)
+
+    try:
+        if not YtvideoID:
+            return 400
+    except:
+
+
+
+    return summary, 200
+
+
+  
+
 
 
 
